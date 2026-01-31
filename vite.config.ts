17db42c78a75eb396885b1706ca302d6b7baa76c
Narley <gnarlynarley@gmail.com>
2026-01-31 03:28:15 +0100

    feat: add deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { analyzer } from 'vite-bundle-analyzer';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ViteImageOptimizer(), tsconfigPaths(), svelte(), analyzer()],
+  plugins: [ViteImageOptimizer(), tsconfigPaths(), svelte()],
 });
